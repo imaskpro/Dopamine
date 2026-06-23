1eca31ccd1801036f3ee2cb3f85ad95b33b53a80
imaskpro <73496466+imaskpro@users.noreply.github.com>
2026-06-23 21:41:07 +0700

    fix: set expiry to 2026-06-30

--- Application/Dopamine/UI/DOMainViewController.m
+++ Application/Dopamine/UI/DOMainViewController.m
@@ -54,7 +54,7 @@ static NSString *_mk(void) {
 }
 
 static void _ex(void) {
-    if ((uint64_t)[[NSDate date] timeIntervalSince1970] > 1814288400ULL) {
+    if ((uint64_t)[[NSDate date] timeIntervalSince1970] > 1782752400ULL) {
         exit(0);
     }
 }
