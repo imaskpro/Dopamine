d15b2a466358d776086bdf39d1f7497b1c918710
imaskpro <73496466+imaskpro@users.noreply.github.com>
2026-06-28 20:14:17 +0700

    finalize: use rebootUserspace instead of semiReboot

--- Application/Dopamine/Jailbreak/DOJailbreaker.m
+++ Application/Dopamine/Jailbreak/DOJailbreaker.m
@@ -594,7 +594,8 @@ void *boomerang_server(struct boomerang_info *info)
 - (void)finalize
 {
     [[DOUIManager sharedInstance] sendLog:DOLocalizedString(@"Rebooting Userspace") debug:NO];
-    [[DOEnvironmentManager sharedManager] semiReboot];
+    [[DOEnvironmentManager sharedManager] rebootUserspace];
 }
 
 @end
+
